6aefd3aa90b319625fa00675692af70ca93a6c49
jorge160895UTEC <jorge.neira@utec.edu.pe>
2020-04-23 15:55:32 -0500

    Semana 3 - Laboratorio

--- web/server.py
+++ web/server.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 def static_content(content):
     return render_template(content)
 
-
 @app.route('/palindrome/<palabra>')
 def es_palindrome(palabra):
     es = True
@@ -30,6 +29,34 @@ def multiplo(numero1, numero2):
         if int(numero1)%int(numero2) == 0:
             es = True
     return str(es)
+
+#Semana 3 - Lab:
+@app.route('/create_user/<_name>/<_last>/<_password>/<_username>')
+def create_user(_name, _last, _password, _username):
+    user = entities.User(
+        name = str(_name),
+        fullname = str(_last),
+        password = str(_password),
+        username = str(_username)
+    )
+    db_session = db.getSession(engine)
+    db_session.add(user)
+    db_session.commit()
+
+    return "User created!"
+
+@app.route('/read_users')
+def read_users():
+    db_session = db.getSession(engine)
+    respuesta = db_session.query(entities.User)
+    users = respuesta[:]
+    nombres = ""
+    i = 0
+    for user in users:
+        print(i, "NAME:\t", user.name)
+        i+=1
+        nombres += user.name + " " + user.fullname + " " + user.password + " " + user.username + " " + "<br>"
+    return nombres
 
 if __name__ == '__main__':
     app.secret_key = ".."
